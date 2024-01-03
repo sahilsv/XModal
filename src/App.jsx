40754c65a6c692ea1d1cc8919c691a3637f34d58
Sahil Singh Virdhi <sahilsv003@gmail.com>
2024-01-03 13:17:10 +0530

    20th Commit

--- src/App.jsx
+++ src/App.jsx
@@ -58,40 +58,39 @@ const App = () => {
   };
 
   return (
-    <div
-      className={`modal`}
-      onClick={handleClickOutside}
-    >
+    <div className='app' onClick={handleClickOutside}>
       <header>User Details Modal</header>
       <button className="submit-button" onClick={openForm}>
         Open Form
       </button>
 
       {isFormOpen && (
-        <div className="modal-content" ref={formRef}>
-          <form>
-            <h2>Fill Details</h2>
-            <label htmlFor="username">Username:</label>
-            <input type="text" id="username" ref={usernameRef} required />
+        <div className="modal" onClick={handleClickOutside}>
+          <div className="modal-content" ref={formRef}>
+            <form>
+              <h2>Fill Details</h2>
+              <label htmlFor="username">Username:</label>
+              <input type="text" id="username" ref={usernameRef} required />
 
-            <label htmlFor="email">Email:</label>
-            <input type="email" id="email" ref={emailRef} required />
+              <label htmlFor="email">Email:</label>
+              <input type="email" id="email" ref={emailRef} required />
 
-            <label htmlFor="phone">Phone Number:</label>
-            <input type="tel" id="phone" ref={phoneRef} required />
+              <label htmlFor="phone">Phone Number:</label>
+              <input type="tel" id="phone" ref={phoneRef} required />
 
-            <label htmlFor="dob">Date of Birth:</label>
-            <input type="date" id="dob" ref={dobRef} required />
+              <label htmlFor="dob">Date of Birth:</label>
+              <input type="date" id="dob" ref={dobRef} required />
 
-            <button
-              type="button"
-              className="submit-button"
-              onClick={handleSubmit}
-            >
-              Submit
-            </button>
-            {/* <div className="submitBtn-div"></div> */}
-          </form>
+              <button
+                type="button"
+                className="submit-button"
+                onClick={handleSubmit}
+              >
+                Submit
+              </button>
+              {/* <div className="submitBtn-div"></div> */}
+            </form>
+          </div>
         </div>
       )}
     </div>
